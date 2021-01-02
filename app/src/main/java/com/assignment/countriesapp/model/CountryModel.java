27ad249c6e966752421888c33faaa27488197939
anand <anand26041997@gmail.com>
2021-01-02 22:02:00 +0530

    Model architecture

--- app/src/main/java/com/assignment/countriesapp/model/CountryModel.java
+++ app/src/main/java/com/assignment/countriesapp/model/CountryModel.java
@@ -1,4 +1,38 @@
 package com.assignment.countriesapp.model;
 
 public class CountryModel {
+
+    public String name;
+    public String capital;
+    public String flag;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public CountryModel(String name, String capital, String flag) {
+        this.name = name;
+        this.capital = capital;
+        this.flag = flag;
+    }
 }
--- app/src/main/java/com/assignment/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/assignment/countriesapp/viewmodel/ListViewModel.java
@@ -1,4 +1,34 @@
 package com.assignment.countriesapp.viewmodel;
 
-public class ListViewModel {
+import com.assignment.countriesapp.model.CountryModel;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ListViewModel extends ViewModel {
+
+    MutableLiveData<List<CountryModel>> country;
+    MutableLiveData<Boolean> countryLoadErr;
+    MutableLiveData<Boolean> loading;
+
+    public void refresh() {
+    }
+
+    public void fetchData() {
+        CountryModel country1 = new CountryModel("India", "Delhi", "");
+        CountryModel country2 = new CountryModel("Nepal", "Kathmandu", "");
+        CountryModel country3 = new CountryModel("Bang", "kL", "");
+
+        List<CountryModel> list = new ArrayList<>();
+        list.add(country1);
+        list.add(country2);
+        list.add(country3);
+        country.setValue(list);
+        countryLoadErr.setValue(false);
+        loading.setValue(false);
+    }
 }
